bab7419382e6f3de68d8ba7cdef501893fdf0645
Geraldina Barrios <geralbm@gmail.com>
2022-05-21 20:00:07 -0300

    09-React-Routing-HW

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About () {
+    return (
+        <div>
+            Hola, esta es la sección About
+        </div>
+    )
+}
+
+export default About
--- 09-React-Routing/homework/src/components/Cards.jsx
+++ 09-React-Routing/homework/src/components/Cards.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Cards.css';
-
 import Card from './Card.jsx';
+
 
 export default function Cards({cities, onClose}) {
   return (
     <div className='cards'>
       {cities.map(c => <Card
           key={c.id}
+          id={c.id}
           max={c.max}
           min={c.min}
           name={c.name}
