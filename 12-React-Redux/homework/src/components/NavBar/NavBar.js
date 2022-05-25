ea230f8c27ec26b3dfe6d8c90d242d9073156f71
Geraldina Barrios <geralbm@gmail.com>
2022-05-25 18:05:34 -0300

    12-React-Redux-HW

--- 12-React-Redux/homework/src/components/NavBar/NavBar.js
+++ 12-React-Redux/homework/src/components/NavBar/NavBar.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Logo from '../../logoHenry.png'
+import React, { useEffect, useRef } from "react";
+import { NavLink } from "react-router-dom";
+import { useCountMoviesFavorites } from "../../hooks/useCountMoviesFavorites";
+import Logo from "../../logoHenry.png";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 export default function NavBar() {
-    return (
-        <header className="navbar">
-            <div>
-                <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
-            </div>
-            <nav>
-                <ul className="list">
-                    <li className="list-item">
-                        <NavLink exact to="/" >Home</NavLink>
-                        <NavLink to="/favs" >Favoritas</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  const { countFav } = useCountMoviesFavorites();
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if(ref.current) {
+        ref.current.src = "https://assets.soyhenry.com/henry-landing/assets/Henry/logo-white.png"
+    }
+  }, [ref]);
+
+  return (
+    <header className="navbar">
+      <div>
+        <img
+          ref={ref}
+          id="logoHenry"
+          src={Logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+        />
+      </div>
+      <nav>
+        <ul className="list">
+          <li className="list-item">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/favs">{`Favoritas (${countFav})`}</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
---
+++ 12-React-Redux/homework/src/hooks/useCountMoviesFavorites.js
@@ -0,0 +1,5 @@
+import { useSelector } from "react-redux";
+export function useCountMoviesFavorites() {
+  const favorites = useSelector((state) => state.moviesFavorites);
+  return { countFav: favorites?.length || 0 };
+}
